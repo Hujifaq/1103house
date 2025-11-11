a00f63f27aa238f7272db68e6db3f5284be0102b
Errmon <thanut2.zoomer@gmail.com>
2025-11-12 02:02:10 +0700

    Footer Added

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,7 +1,22 @@
 import NavbarComponent from './nav.js';
+import footercom, { initFooter } from './footer.js';
+
+// Create Footer Component class
+class FooterComponent extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = footercom();
+        if (typeof initFooter === 'function') {
+            try { initFooter(this); } catch (e) { /* ignore */ }
+        }
+    }
+}
 
 // Register the navbar component immediately
 if (!customElements.get('navbar-component')) {
-customElements.define('navbar-component', NavbarComponent);
+    customElements.define('navbar-component', NavbarComponent);
 }
 
+// Register the footer component
+if (!customElements.get('footer-component')) {
+    customElements.define('footer-component', FooterComponent);
+}
